b5200c7886ac4e428e381ce18cb63ab5a389860c
afabiszewski <adrian.fabiszewski@outlook.com>
2024-06-04 22:40:50 +0200

    fix returned dummy value

--- src/main/kotlin/DbGeneralEventRepository.kt
+++ src/main/kotlin/DbGeneralEventRepository.kt
@@ -1,10 +1,11 @@
 import java.util.UUID
+import kotlin.collections.HashMap
 
-object DbGeneralEventRepository : EventRepository{
+object DbGeneralEventRepository : EventRepository {
 
     lateinit var map: HashMap<UUID, Event>
 
-    override fun save(event: Event): Int {
-        TODO("Not yet implemented")
-    }
+    override fun save(event: Event): Int = 1
+        //TODO("Not yet implemented")
+
 }
